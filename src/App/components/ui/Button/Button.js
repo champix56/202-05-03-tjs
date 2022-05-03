40f73868d8c8e8870909e36cfa0fab48cba3a8a8
champix56 <desorbaix@free.fr>
2022-05-03 15:18:11 +0200

    cycle de vie useEffect sur func

--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Button.module.css";
 import PropTypes from "prop-types";
 
 const Button = (props) => {
   const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => {
+    console.log('isClicked a changer ->',isClicked)
+  }, [isClicked])
   return (
     <button
       className={style.Button}
