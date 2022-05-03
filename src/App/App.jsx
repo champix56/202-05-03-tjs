bfa13a6f93373950a8d50969e8cb3000416db0b5
champix56 <desorbaix@free.fr>
2022-05-03 12:28:42 +0200

    css module

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "./components/ui/Button/Button";
+import Button2 from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
-     
+      <Button></Button>
+      <Button> </Button>
+      <Button></Button>
     </div>
   );
 }
