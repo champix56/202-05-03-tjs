161f1185de8a38a9406c6343dc6a6b99a2c70f97
champix56 <desorbaix@free.fr>
2022-05-03 15:04:46 +0200

    gestion d'etat avec update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import "./App.css";
 import Button from "./components/ui/Button/Button";
-
-function App() {
-  return (
-    <div className="App">
-      {/* <Button text="Un texte de button"></Button> */}
-      <Button bgColor="skyblue">du texte children</Button>
-      <Button bgColor="aquamarine">
-        <div>text children node</div>
-      </Button>
-      <Button
-        onButtonClicked={() => {
-          console.log("button clicked");
-        }}
-        color="blue"
-        style={{
-          textDecoration: "underline",
-          fontWeight: 900,
-          fontStyle: "italic",
-        }}
-      >
-        <div>text children node1</div>
-        <div>text children node2</div>
-      </Button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 1, title: "titre de l'app" };
+  }
+  componentDidUpdate(oldProps, oldSate) {
+    console.log("component update", oldSate, this.state);
+  }
+  render() {
+    return (
+      <div className="App">
+        Valeur du counter : {this.state.counter}
+        <hr />
+        <Button
+          onButtonClicked={() => {
+            this.setState({ counter: this.state.counter + 1 });
+          }}
+          bgColor="skyblue"
+        >
+          Ajouter 1
+        </Button>
+        <Button
+          onButtonClicked={() => {
+            this.setState({ counter: this.state.counter - 1 });
+          }}
+          bgColor="tomato"
+        >
+          Enlever 1
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
