11182bc2de6d0008d2ba0c533858fcca736f15e4
champix56 <desorbaix@free.fr>
2022-05-04 10:57:17 +0200

    mise en place du cmp de orsystjsmeme memeviewer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import Button from "./components/ui/Button/Button";
+import FlexW from "./components/layout/FlexW/FlexW";
+import { MemeSVGViewer } from "orsys-tjs-meme";
 import { ADR_REST } from "./config/config";
+import MemeForm from "./components/ui/MemeForm/MemeForm";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { memes:[], images: [] };
+    this.state = { memes: [], images: [] };
   }
-  componentDidMount(){
-    const memes=  fetch(ADR_REST+'/memes').then(flux=>flux.json())
-    const img =  fetch(ADR_REST+'/images').then(flux=>flux.json())
-    Promise.all([memes,img]).then(arr=>this.setState({memes:arr[0],images:arr[1]}))
+  componentDidMount() {
+    const memes = fetch(ADR_REST + "/memes").then((flux) => flux.json());
+    const img = fetch(ADR_REST + "/images").then((flux) => flux.json());
+    Promise.all([memes, img]).then((arr) =>
+      this.setState({ memes: arr[0], images: arr[1] })
+    );
   }
   componentDidUpdate(oldProps, oldSate) {
     console.log("component update", oldSate, this.state);
@@ -17,7 +21,16 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-       {JSON.stringify(this.state)}
+        <FlexW>
+          {this.state.memes.length &&
+            <MemeSVGViewer
+            meme={this.state.memes[0]}
+            image={this.state.images.find(
+              (e) => e.id === this.state.memes[0].imageId
+            )}
+          />}
+          <MemeForm/>
+        </FlexW>
       </div>
     );
   }
