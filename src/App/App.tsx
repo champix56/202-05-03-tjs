2baabc66f423418c369e4647f7363c85fd5a2e77
champix56 <desorbaix@free.fr>
2022-05-05 09:38:25 +0200

    correction de App face aux connections magasin

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,41 +1,18 @@
-import React from "react";
 import FlexW from "./components/layout/FlexW/FlexW";
-import MemeSVGViewer from './components/ui/ConnectedMemeViewer/ConnectedMemeViewer'
-import { ADR_REST } from "./config/config";
-import MemeForm, { ConnectedMemeForm } from "./components/ui/MemeForm/MemeForm";
-import { IImage, IMeme } from "orsys-tjs-meme/dist/interfaces/common";
-import { DummyMeme } from "./interfaces/common";
+import MemeSVGViewer from "./components/ui/ConnectedMemeViewer/ConnectedMemeViewer";
+import { ConnectedMemeForm } from "./components/ui/MemeForm/MemeForm";
 import { ConnectedMemeThumbnail } from "./components/ui/MemeThumbnail/MemeThumbnail";
-import {ACTION_RESSOURCES, store} from './store/store'
-interface IAppState {
-  memes: Array<IMeme>;
-  images: Array<IImage>;
-  current: IMeme;
-}
-interface IAppProps {}
-class App extends React.Component<IAppProps, IAppState> {
-  constructor(props) {
-    super(props);
-    this.state = { memes: [], images: [], current: DummyMeme };
-  }
-  componentDidMount() {
-    
-  }
-  componentDidUpdate(oldProps, oldSate) {
-    console.log("component update", oldSate, this.state);
-  }
-  render() {
-    return (
-      <div className="App">
-        {/* <MemeThumbnail memes={this.state.memes} images={this.state.images}/> */}
-        <ConnectedMemeThumbnail/>
-        <FlexW>
-          <MemeSVGViewer/>
-          <ConnectedMemeForm/>
-        </FlexW>
-      </div>
-    );
-  }
-}
+
+const App = () => {
+  return (
+    <div className="App">
+      <ConnectedMemeThumbnail />
+      <FlexW>
+        <MemeSVGViewer />
+        <ConnectedMemeForm />
+      </FlexW>
+    </div>
+  );
+};
 
 export default App;
