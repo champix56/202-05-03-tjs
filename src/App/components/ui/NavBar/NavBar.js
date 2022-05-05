c820b7dfb91fa5e261f5ee81bf00168cd4e80067
champix56 <desorbaix@free.fr>
2022-05-05 12:05:25 +0200

    Link avec react-router-bootstrap

--- src/App/components/ui/NavBar/NavBar.js
+++ src/App/components/ui/NavBar/NavBar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import style from "./NavBar.module.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
+import { LinkContainer } from "react-router-bootstrap";
+
 const NavBar = (props) => {
   return (
     <div className={style.NavBar} data-testid="NavBar">
@@ -9,11 +11,21 @@ const NavBar = (props) => {
         <Container>
           <Navbar.Brand href="#home">Meme Generator</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">New</Nav.Link>
-            <Nav.Link href="#edit">Edit/1</Nav.Link>
-            <Nav.Link href="#pricing">Thumbnail</Nav.Link>
-            <Nav.Link href="#thumbnail">ThumbnailPDF</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/editor">
+              <Nav.Link>New</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/editor/1">
+              <Nav.Link>Edit/1</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/thumbnail">
+              <Nav.Link>Thumbnail</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/thumbnailPDF">
+              <Nav.Link>ThumbnailPDF</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
