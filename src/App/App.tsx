245481e5420a6395145e8953b9f9eb77e8c8cd08
champix56 <desorbaix@free.fr>
2022-05-05 15:41:25 +0200

    selection par route du current

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,6 +7,8 @@ import { Routes, Route, useParams, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { ACTIONS_CURRENT, ACTION_RESSOURCES, store } from "./store/store";
 import { PDFConnectedMemeThumbnail } from "./components/pdf/PDFThumbnail/PDFThumbnail";
+import { connect } from "react-redux";
+import { IMeme } from "orsys-tjs-meme/dist/interfaces/common";
 
 const App = () => {
   return (
@@ -14,21 +16,44 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<div>Hello a tous</div>} />
-        <Route path="/editor" element={<MemeEditor/>} />
-        <Route path="/editor/:id" element={<MemeEditor/>} />
+        <Route path="/editor" element={<MemeEditor />} />
+        <Route path="/editor/:id" element={<MemeEditor />} />
         <Route path="/thumbnail" element={<ConnectedMemeThumbnail />} />
-        <Route path="/thumbnailPDF" element={<PDFConnectedMemeThumbnail/>} />
+        <Route path="/thumbnailPDF" element={<PDFConnectedMemeThumbnail />} />
       </Routes>
     </div>
   );
 };
-function MemeEditor(props:any) {
+function mstp(s, o) {
+  return { memes: s.ressources.memes };
+}
+function mdtp(d: Function) {
+  return {
+    select: (meme?:IMeme) => {
+      console.log("hello", meme, store.getState().ressources);
+
+      if (undefined !== meme) {
+        d({ type: ACTIONS_CURRENT.UPDATE_MEME, value: meme });
+      } else {
+        d({ type: ACTIONS_CURRENT.CLEAR_CURRENT });
+      }
+    },
+  };
+}
+const MemeEditor = connect(
+  mstp,
+  mdtp
+)(function (props: any) {
+  let id = useParams().id;
+  useEffect(() => {
+    props.select(props.memes.find(m=>m.id===Number(id)));
+  }, [props]);
   return (
     <FlexW>
       <MemeSVGViewer />
       <ConnectedMemeForm />
     </FlexW>
   );
-}
+});
 
 export default App;
